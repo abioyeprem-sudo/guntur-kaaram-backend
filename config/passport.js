6f14b8892dcd806058ae41a07156cab90eaf42b4
abioyeprem-sudo <abioyeprem@gmail.com>
2026-09-07 01:45:21 +0530

    Implement Google and Facebook authentication strategies

---
+++ config/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const User = require('../models/User');
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    const email = profile.emails?.[0]?.value;
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await User.create({
+        name: profile.displayName,
+        email,
+        avatar: profile.photos?.[0]?.value,
+        provider: 'google',
+        providerId: profile.id
+      });
+    }
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+}));
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+  profileFields: ['id', 'displayName', 'emails', 'photos']
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    const email = profile.emails?.[0]?.value || `${profile.id}@facebook.placeholder`;
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await User.create({
+        name: profile.displayName,
+        email,
+        avatar: profile.photos?.[0]?.value,
+        provider: 'facebook',
+        providerId: profile.id
+      });
+    }
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+}));
+
+module.exports = passport;
